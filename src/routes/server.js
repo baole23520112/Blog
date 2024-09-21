6cfcb3a4a1c9262d4894a3d77b073229eeb6568b
baole23520112 <23520112@gm.uit.edu.vn>
2024-09-21 20:48:35 +0700

    Delete news route, news controller

--- src/routes/server.js
+++ src/routes/server.js
@@ -1,12 +1,8 @@
-import newsRouter from './news.js';
 import siteRouter from './site.js';
 import articleRouter from './articles.js'
 import meRouter from './me.js'
 // Combine route + branch and function handler
 function route(app) {
-
-	// Match to /news
-	app.use('/news', newsRouter);
 
 	app.use('/articles', articleRouter);
 

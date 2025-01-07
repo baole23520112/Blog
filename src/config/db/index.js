55ab58bedd0be10d8af3a47a984b81c13cc7e081
lvb2104 <levanbao2105@gmail.com>
2025-01-07 11:19:44 +0700

    fix: change connection string to mongodb

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 // Established connection
 async function connect() {
 	try {
-		await mongoose.connect('mongodb://localhost:27017/blog');
+		await mongoose.connect('mongodb+srv://moda:moda123@cluster0.d6sfz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 		console.log('CONNECT SUCCESSFULLY');
 	} catch (error) {
 		console.error('FAIL TO CONNECT');
